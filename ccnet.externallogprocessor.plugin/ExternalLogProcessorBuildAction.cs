26f09a7ee5f45bb69270a9ec0f7cc64576976a33
Ace Olszowka <aolszowka@gmail.com>
2018-06-17 09:38:18 -0600

    * Initial commit

---
+++ ccnet.externallogprocessor.plugin/ExternalLogProcessorBuildAction.cs
@@ -0,0 +1,91 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ExternalLogProcessorBuildAction.cs" company="Ace Olszowka">
+// Copyright (c) 2018 Ace Olszowka.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ccnet.externallogprocessor.plugin
+{
+    using System;
+    using System.Diagnostics;
+    using Exortech.NetReflector;
+    using ThoughtWorks.CruiseControl.WebDashboard.Dashboard;
+    using ThoughtWorks.CruiseControl.WebDashboard.IO;
+    using ThoughtWorks.CruiseControl.WebDashboard.MVC;
+    using ThoughtWorks.CruiseControl.WebDashboard.MVC.Cruise;
+
+    /// <summary>
+    /// A CruiseControl.NET Build Action that will pass the entire contents
+    /// of the build log as Standard Input to a specified program and then
+    /// take the output of that program and display it in the dashboard.
+    /// </summary>
+    [ReflectorType("externalLogProcessorBuildAction")]
+    public class ExternalLogProcessorBuildAction : ICruiseAction, IConditionalGetFingerprintProvider
+    {
+        private readonly IFingerprintFactory fingerprintFactory;
+        private readonly IBuildRetriever buildRetriever;
+
+        public ExternalLogProcessorBuildAction(IBuildRetriever buildRetriever, IFingerprintFactory fingerprintFactory)
+        {
+            this.fingerprintFactory = fingerprintFactory;
+            this.buildRetriever = buildRetriever;
+        }
+
+        /// <summary>
+        /// Gets or sets the fully qualified path to the external log processor application.
+        /// </summary>
+        [ReflectorProperty("externalLogProcessor")]
+        public string ExternalLogProcessor { get; set; }
+            = string.Empty;
+
+        /// <summary>
+        /// Grabs the associated log then executes the specified External
+        /// Program passing the log as Standard Input. The Standard Output
+        /// is then pushed back as an HtmlFragment.
+        /// </summary>
+        /// <param name="cruiseRequest">The Cruise Request.</param>
+        /// <returns><seealso cref="HtmlFragmentResponse"/> with the output.</returns>
+        public IResponse Execute(ICruiseRequest cruiseRequest)
+        {
+            string log = buildRetriever.GetBuild(cruiseRequest.BuildSpecifier).Log;
+
+            string result = "Failed To Execute External Program";
+
+            try
+            {
+                using (Process externalProgram = new Process())
+                {
+                    externalProgram.StartInfo.FileName = ExternalLogProcessor;
+                    externalProgram.StartInfo.UseShellExecute = false;
+                    externalProgram.StartInfo.RedirectStandardInput = true;
+                    externalProgram.StartInfo.RedirectStandardOutput = true;
+
+                    externalProgram.Start();
+                    externalProgram.StandardInput.Write(log);
+                    externalProgram.StandardInput.Close();
+                    result = externalProgram.StandardOutput.ReadToEnd();
+
+                    externalProgram.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                // If we fail for any reason just toally bail out
+                result = string.Format("Exception Encountered When Executing Program: {0}", ex.Message);
+            }
+
+            return new HtmlFragmentResponse(result);
+        }
+
+        // <HACK>
+        // I really don't know what this does; I suspect its something to do
+        // with telling CruiseControl.NET how to handle caching, but its
+        // its unclear. I do know its needed for HtmlFragmentResponse to work
+        // though (it needs the IConditionalGetFingerPrintProvider interface)
+        // </HACK>
+        public ConditionalGetFingerprint GetFingerprint(IRequest request)
+        {
+            return fingerprintFactory.BuildFromDate(DateTime.Now);
+        }
+    }
+}
---
+++ ccnet.externallogprocessor.plugin/ExternalLogProcessorBuildPlugin.cs
@@ -0,0 +1,67 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ExternalLogProcssorBuildPlugin.cs" company="Ace Olszowka">
+// Copyright (c) 2018 Ace Olszowka.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ccnet.externallogprocessor.plugin
+{
+    using Exortech.NetReflector;
+    using ThoughtWorks.CruiseControl.WebDashboard.Dashboard;
+    using ThoughtWorks.CruiseControl.WebDashboard.Dashboard.GenericPlugins;
+
+    /// <summary>
+    /// A CruiseControl.NET Build Plugin that will pass the entire contents
+    /// of the build log as Standard Input to a specified program and then
+    /// take the output of that program and display it in the dashboard.
+    /// </summary>
+    [ReflectorType("externalLogProcessorBuildPlugin")]
+    public class ExternalLogProcessorBuildPlugin : ProjectConfigurableBuildPlugin
+    {
+        private readonly IActionInstantiator actionInstantiator;
+
+        public ExternalLogProcessorBuildPlugin(IActionInstantiator actionInstantiator)
+        {
+            this.actionInstantiator = actionInstantiator;
+        }
+
+        /// <summary>
+        /// Gets or sets the property that will be used as the Link Description in CruiseControl.NET
+        /// </summary>
+        [ReflectorProperty("description")]
+        public string ConfiguredLinkDescription { get; set; }
+            = "No Description Configured. Check your dashboard.config";
+
+        /// <summary>
+        /// Gets or sets the name of the action (which will be the Link Name) in CruiseControl.NET
+        /// </summary>
+        [ReflectorProperty("actionName")]
+        public string ActionName { get; set; }
+            = "NoActionSetCheckDashboardConfig";
+
+        /// <summary>
+        /// Gets or sets the fully qualified path to the external log processor application.
+        /// </summary>
+        [ReflectorProperty("externalLogProcessor")]
+        public string ExternalLogProcessor { get; set; }
+            = string.Empty;
+
+        public override INamedAction[] NamedActions
+        {
+            get
+            {
+                ExternalLogProcessorBuildAction action = (ExternalLogProcessorBuildAction)actionInstantiator.InstantiateAction(typeof(ExternalLogProcessorBuildAction));
+                action.ExternalLogProcessor = ExternalLogProcessor;
+                return new INamedAction[] { new ImmutableNamedAction(ActionName, action) };
+            }
+        }
+
+        /// <summary>
+        /// Gets the Link Description that will be displayed by CruiseControl.NET. See <see cref="ConfiguredLinkDescription"/>.
+        /// </summary>
+        public override string LinkDescription
+        {
+            get { return ConfiguredLinkDescription; }
+        }
+    }
+}
